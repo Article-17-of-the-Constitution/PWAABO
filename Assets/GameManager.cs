0debf5417588af7874b815ffdfeb0fd3e21c9c91
FrostKim0208 <admiral0208@naver.com>
2023-12-04 19:41:25 +0900

    [Modify] Scripts & Assets
    Gamemanager - delete unnecessary method & add difiiculty level method
    Ball - add brick collission method
    delete Aimrotation.cs

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,42 +5,82 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public DifficultyManager.DifficultyLevel currentDifficulty;
+    private float defaultTimeLimit = 60f;
+
+    private float startTime;
+    private float elapsedTime;
+
+    private float easyDropRate = 0.1f;
+    private float hardDropRate = 0.05f;
+
+    public Sprite itemSprite;
+
     [Header("Ball")]
     public Ball ball;
 
-    [Header("Player 1")]
-    public Paddle player1Paddle;
-    public Goal player1Goal;
-
-    [Header("Player 2")]
-    public Paddle player2Paddle;
-    public Goal player2Goal;
 
     [Header("UI")]
     public TextMeshProUGUI player1Text;
     public TextMeshProUGUI player2Text;
 
-    private int player1Score;
-    private int player2Score;
-
-    public void Player1Scored()
+    void Start()
     {
-        player1Score++;
-        player1Text.text = player1Score.ToString();
-        ResetPosition();
+        startTime = Time.time;
+        currentDifficulty = DifficultyManager.SelectedDifficulty;
+        SetTimeLimit();
+    }
+    void Update()
+    {
+        elapsedTime = Time.time - startTime;
+
+        if (elapsedTime >= defaultTimeLimit)
+        {
+            EndGame();
+        }
+    }
+    void SetTimeLimit()
+    {
+        switch (currentDifficulty)
+        {
+            case DifficultyManager.DifficultyLevel.Easy:
+                defaultTimeLimit = 60f;
+                break;
+            case DifficultyManager.DifficultyLevel.Hard:
+                defaultTimeLimit = 30f;
+                break;
+        }
+        Debug.Log("제한 시간이 " + defaultTimeLimit + "초로 설정되었습니다.");
+    }
+    void EndGame()
+    {
+        Debug.Log("게임 종료! 경과 시간: " + elapsedTime + "초");
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            float dropRate = (currentDifficulty == DifficultyManager.DifficultyLevel.Easy) ? easyDropRate : hardDropRate;
+
+            if (Random.value <= dropRate)
+            {
+                SpawnItem();
+            }
+            Destroy(gameObject);
+        }
+    }
+    void SpawnItem()
+    {
+        Vector3 spawnPosition = transform.position;
+
+        GameObject item = new GameObject("Item");
+        item.transform.position = spawnPosition;
+
+        SpriteRenderer spriteRenderer = item.AddComponent<SpriteRenderer>();
+        spriteRenderer.sprite = itemSprite;
+
+        Debug.Log("아이템이 생성되었습니다!");
     }
 
-    public void Player2Scored()
-    {
-        player2Score++;
-        player2Text.text = player2Score.ToString();
-        ResetPosition();
-    }
 
-    private void ResetPosition()
-    {
-        ball.Reset();
-        player1Paddle.Reset();
-        player2Paddle.Reset();        
-    }
 }
