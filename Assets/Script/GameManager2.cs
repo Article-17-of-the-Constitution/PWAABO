a9bc737fbfa25e7eb756ddceec92d1abc75c3240
KNH <reiraps@naver.com>
2023-12-04 10:54:04 +0900

    DifficultyManager, GameManager2 추가
    난이도 스크립트

---
+++ Assets/Script/GameManager2.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager2 : MonoBehaviour
+{
+    public DifficultyManager.DifficultyLevel currentDifficulty;
+
+    private float defaultTimeLimit = 60f;
+
+    private float startTime;
+    private float elapsedTime;
+
+    private float easyDropRate = 0.1f;
+    private float hardDropRate = 0.05f;
+
+    public Sprite itemSprite;
+
+    void Start()
+    {
+        startTime = Time.time;
+        currentDifficulty = DifficultyManager.SelectedDifficulty;
+        SetTimeLimit();
+    }
+    void Update()
+    {
+        elapsedTime = Time.time - startTime;
+
+        if (elapsedTime >= defaultTimeLimit)
+        {
+            EndGame();
+        }
+    }
+    void SetTimeLimit()
+    {
+        switch (currentDifficulty)
+        {
+            case DifficultyManager.DifficultyLevel.Easy:
+                defaultTimeLimit = 60f;
+                break;
+            case DifficultyManager.DifficultyLevel.Hard:
+                defaultTimeLimit = 30f;
+                break;
+        }
+        Debug.Log("제한 시간이 " + defaultTimeLimit + "초로 설정되었습니다.");
+    }
+    void EndGame()
+    {
+        Debug.Log("게임 종료! 경과 시간: " + elapsedTime + "초");
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            float dropRate = (currentDifficulty == DifficultyManager.DifficultyLevel.Easy) ? easyDropRate : hardDropRate;
+
+            if (Random.value <= dropRate)
+            {
+                SpawnItem();
+            }
+            Destroy(gameObject);
+        }
+    }
+    void SpawnItem()
+    {
+        Vector3 spawnPosition = transform.position;
+
+        GameObject item = new GameObject("Item");
+        item.transform.position = spawnPosition;
+
+        SpriteRenderer spriteRenderer = item.AddComponent<SpriteRenderer>();
+        spriteRenderer.sprite = itemSprite;
+
+        Debug.Log("아이템이 생성되었습니다!");
+    }  
+}
