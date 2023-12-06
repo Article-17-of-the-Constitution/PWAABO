c20b35b6945c72fb74737549a1bfdec36b4cb571
민현영 <alsgsudud077@naver.com>
2023-12-06 17:58:55 +0900

    button

--- Assets/Scripts/StartBtn.cs
+++ Assets/Scripts/StartBtn.cs
@@ -3,21 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class StartBtn : MonoBehaviour
 {
     public void GameStart()
     {
         SceneManager.LoadScene("CharacterChoiceScene");
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
