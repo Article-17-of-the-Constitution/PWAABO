32ae061a874bbd75e46d5cf2052dec689615a1ef
민현영 <alsgsudud077@naver.com>
2023-12-06 16:24:39 +0900

    SceneImage

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -13,6 +13,31 @@ public class MainMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    public void OnClickGameStart() 
+    {
+
+        Debug.Log("게임시작");
+    }
+    public void OnClickCharacterChoice() 
+    {
+        Debug.Log("캐릭터 선택");
+    }
+    public void OnClickLevelChoiceScene() 
+    {
+        Debug.Log("난이도 선택");
+    }
+    public void OnClickOption() 
+    {
+        Debug.Log("옵션");
+    }
+
+    public void OnClickQuit() 
+    {
+        Debug.Log("");
+
     }
 }
+
+
