9092793083cdf8e1f38c579605d37ebd3b65e470
FrostKim0208 <admiral0208@naver.com>
2023-12-06 17:07:57 +0900

    Divide Scenes

--- Assets/Scripts/StartBtn.cs
+++ Assets/Scripts/StartBtn.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameScenesMove : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
+    public void GameStart()
+    {
+        SceneManager.LoadScene("CharacterChoiceScene");
+    }
     // Start is called before the first frame update
     void Start()
     {
