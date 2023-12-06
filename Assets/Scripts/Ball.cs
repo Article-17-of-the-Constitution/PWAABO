4966ea96f8aa333863b30d7b1e47fc486c9c0d55
FrostKim0208 <admiral0208@naver.com>
2023-12-06 20:21:57 +0900

    Modify Ball Script

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,21 +5,17 @@ using UnityEngine.SocialPlatforms.Impl;
 
 public class Ball : MonoBehaviour
 {
-<<<<<<< Updated upstream
     public float speed;
     public Rigidbody2D rigidbody;
     public AudioClip DM_CGS_44;
     public AudioSource audioSource;
 
 
-=======
     public float ballspeed = 10f;
 
     private Vector2 balldirection;
     private bool isballreleased = false;
     
-    // Start is called before the first frame update
->>>>>>> Stashed changes
     void Start()
     {
         balldirection = Vector2.up.normalized;
@@ -28,29 +24,6 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< Updated upstream
-        float x = Random.Range(0, 2) == 0 ? -1 : 1;
-        float y = Random.Range(0, 2) == 0 ? -1 : 1;
-
-        rigidbody.velocity = new Vector2(x * speed, y * speed);
-    }
-
-
-
-    public void Reset()
-    {
-        rigidbody.velocity = Vector2.zero;
-        transform.position = Vector2.zero;
-        Launch();
-    }
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-
-        if (collision.gameObject.tag == "brick")
-        {
-            audioSource.PlayOneShot(DM_CGS_44);
-            Destroy(collision.gameObject);
-=======
         if (!isballreleased)
         {
             Vector3 paddleposition = GameObject.Find("Paddle").transform.position;
@@ -64,15 +37,29 @@ public class Ball : MonoBehaviour
                 isballreleased = true;
                 balldirection = new Vector2(Random.Range(-1f, 1f), 1).normalized;
             }
->>>>>>> Stashed changes
+
         }
         else
         {
-            transform.Translate(balldirection*ballspeed*Time.deltaTime);
+            transform.Translate(balldirection * ballspeed * Time.deltaTime);
         }
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+
+
+
+    public void Reset()
     {
+        rigidbody.velocity = Vector2.zero;
+        transform.position = Vector2.zero;
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "brick")
+        {
+            audioSource.PlayOneShot(DM_CGS_44);
+            Destroy(collision.gameObject);
+        }
         if(collision.gameObject.CompareTag("Wall"))
         {
             balldirection = Vector2.Reflect(balldirection, collision.contacts[0].normal);
